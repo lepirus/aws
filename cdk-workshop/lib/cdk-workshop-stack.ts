07d3f1b29d640bb42454fc2f5183e63b0d45a615
Alberto Gordillo <lepirus@gmail.com>
2022-09-01 18:21:29 +0200

    Add API Gateway REST to HelloHandler Lambda

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,16 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // defines an AWS Lambda resource
-    new lambda.Function(this, 'HelloHandler', {
+    const helloLambda = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
       handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function
+    new apigw.LambdaRestApi(this, 'Endpoint CdkWorkshop', {
+      handler: helloLambda
     });
   }
 }
