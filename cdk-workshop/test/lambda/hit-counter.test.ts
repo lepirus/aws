4900ff5e5c63962144f17fc531cdd946965879f2
Alberto Gordillo <lepirus@gmail.com>
2022-09-05 12:15:05 +0200

    Added Encryption to DynamoDB + test

--- cdk-workshop/test/lambda/hit-counter.test.ts
+++ cdk-workshop/test/lambda/hit-counter.test.ts
@@ -3,6 +3,27 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { HitCounter } from '../../lib/hit-counter';
 import { Capture, Template } from 'aws-cdk-lib/assertions';
 
+test('DynamoDB Table Created With Encryption', () => {
+    const stack = new cdk.Stack();
+    // WHEN
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream:  new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hello.handler'
+        })
+    });
+
+    // THEN
+    const template = Template.fromStack(stack);
+    template.resourceCountIs("AWS::DynamoDB::Table", 1);
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+        SSESpecification: {
+            SSEEnabled: true
+        }
+    });
+});
+
 test('DynamoDB Table Created', () => {
     const stack = new cdk.Stack();
     // WHEN
